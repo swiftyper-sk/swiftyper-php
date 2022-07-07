f24407ecfc60f5d103ac9ad605684e14503f09d6
Richard Dobroň <office.etexweb@gmail.com>
2022-07-07 19:14:59 +0200

    🔖 Release 1.0.4

--- lib/Swiftyper.php
+++ lib/Swiftyper.php
@@ -31,7 +31,7 @@ class Swiftyper
     /** @var float Initial delay between retries, in seconds */
     private static $initialNetworkRetryDelay = 0.5;
 
-    const VERSION = '1.0.3';
+    const VERSION = '1.0.4';
 
     /**
      * @return string the API key used for requests
